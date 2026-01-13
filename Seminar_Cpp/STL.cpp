1008f173cabc0a8cd0dbfe977dd3c2fc5e572d0b
Peter Loos <peter.loos@gmx.de>
2026-01-13 16:41:48 +0100

    Teufel

--- Seminar_Cpp/STL.cpp
+++ Seminar_Cpp/STL.cpp
@@ -2,9 +2,9 @@
 #include <print>
 #include <deque>
 #include <list>
+#include <algorithm>
 
-
-void test_stl_01()
+static void test_stl_01()
 {
     std::vector<int> numbers;
 
@@ -20,7 +20,7 @@ void test_stl_01()
     }
 }
 
-void test_stl_02()
+static void test_stl_02()
 {
     std::vector<int> numbers;
 
@@ -38,7 +38,7 @@ void test_stl_02()
 }
 
 
-void test_stl_03()
+static void test_stl_03()
 {
    // std::vector<int> numbers;
     // std::deque<int> numbers;
@@ -94,7 +94,7 @@ void test_stl_03()
 }
 
 
-void test_stl_04()
+static void test_stl_04()
 {
     std::vector<int> numbers;
 
@@ -112,7 +112,128 @@ void test_stl_04()
     }
 }
 
+static void print(int value)
+{
+    std::println(" ==> {}", value);
+}
+
+static void test_stl_05()
+{
+    std::vector<int> numbers;
+
+    numbers.push_back(10);
+    numbers.push_back(11);
+    numbers.push_back(12);
+
+    // Funktion:  std::for_each 
+
+    std::for_each(
+        numbers.begin(),
+        numbers.end(),
+        print
+    );
+}
+
+static void test_stl_06()
+{
+    std::vector<int> numbers;
+
+    numbers.resize(50);
+
+    // Vorbelegung
+    // Ohne STL Unterstützung
+    //for (int i = 0; i != size; ++i) {
+    //    m_data[i] = 0;
+    //}
+
+    std::fill(
+        numbers.begin(),
+        numbers.end(),
+        123
+    );
+}
+
+
+static int init()
+{
+    // return 456;
+    static int value = 1;    // value has global state, but is only visible inside function 'init'
+    value += 2;
+    return value;
+}
+
+static void test_stl_07()
+{
+    std::vector<int> numbers;
+
+    numbers.resize(50);
+
+    std::generate(
+        numbers.begin(),
+        numbers.end(),
+        init
+    );
+}
+
+// =======================================================
+
+class Printer
+{
+public:
+    int operator() (int value) {
+        return 2 * value;
+    }
+};
+
+static void test_stl_08()
+{
+    Printer myPrinter;
+
+    int result = myPrinter(10);
+}
+
+
+class FormattedPrinter
+{
+private:
+    std::string m_header;
+
+public:
+    FormattedPrinter() {
+        m_header = ">>> ";
+    }
+
+    FormattedPrinter(const std::string& header) {
+        m_header = header;
+    }
+
+    void operator() (int value) {
+        std::println("{} {}", m_header, value);
+    }
+};
+
+static void test_stl_09()
+{
+    std::vector<int> numbers;
+    numbers.resize(10);
+
+    std::fill(
+        numbers.begin(),
+        numbers.end(),
+        123
+    );
+
+    FormattedPrinter myPrinter ("========>   ");
+
+    std::for_each(
+        numbers.begin(),
+        numbers.end(),
+        myPrinter
+    );
+}
+
+
 void test_stl()
 {
-    test_stl_04();
+    test_stl_09();
 }
